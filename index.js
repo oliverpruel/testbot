5fb67f2356b11569ea0859accd9fdbe459ed0321
Oliver Pruel <oliver.pruel@bigbank.ee>
2017-01-29 15:52:43 +0200

    testing

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
+  console.log('Request:: ' + req);
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
